85af760154cb986596fa121ac6dc9a2d1504af42
Thomas Phil <Sikerdebaard@users.noreply.github.com>
2021-07-05 08:57:49 +0200

    Update printqr.py

--- examples/printqr.py
+++ examples/printqr.py
@@ -1,11 +1,8 @@
 from coronacheck_tools import decode_qr
 from pprint import pprint
 
-from base64 import b64encode
 
 # default format = dict, so it decodes everything to a dict
-data = decode_qr('test/testdata/qrtest2.png', "raw")
+data = decode_qr('test/testdata/qrtest2.png')
 
-#pprint(data)
-print(b64encode(data[0].encode()))
-
+pprint(data)
