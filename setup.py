f63c6ea9976c0447603d275f95750e1a15f069d6
t.phil <thomas@tphil.nl>
2021-11-08 12:07:35 +0100

    Fixing the raspberry pi build

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coronacheck-tools',
-    version='4',
-    description='Unofficial tool to encode, decode and verify the QR code from CoronaCheck.nl. Not affiliated with CoronaCheck.nl or Ministry of VWS.',  # noqa: E501
+    version='5',
+    description='Unofficial tool to encode, decode and verify the domestic QR code from CoronaCheck.nl. Not affiliated with CoronaCheck.nl or Ministry of VWS.',  # noqa: E501
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Thomas Phil',
@@ -25,7 +25,7 @@ setup(
         'cleo>=0.8.1',
         'base58>=2.1.0',
         'pyzbar>=0.1.8',
-        'opencv-python>=4.3.0.38',
+        'opencv-python==4.5.3.56',  # afix opencv to a version with raspberry pi compatible wheels
         'asn1tools>=0.158.0',
         'qrcode>=6.1',
         'Pillow>=8.2.0',
